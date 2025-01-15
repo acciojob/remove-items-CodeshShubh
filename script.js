5a5594a1b23a7dee59238f8b78f51aa09c13bdcf
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-16 05:14:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let select = document.getElementById('colorSelect');
+
+let btn = document.querySelector('input[type="button"][value ="Select and Remove"]');
+btn.addEventListener('click', ()=>{
+	if(select.selectedIndex !==-1){
+		select.remove(select.selectedIndex);
+	}else{
+		alert('No option selected to remove!') 
+	}
+})
